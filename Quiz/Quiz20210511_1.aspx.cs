9f72dc5bdd3f77744a33d63c6dd524416841b956
益宏 劉 <107111224@mail.oit.edu.tw>
2021-05-11 17:05:33 +0800

    隨堂2

--- Quiz/Quiz20210511_1.aspx.cs
+++ Quiz/Quiz20210511_1.aspx.cs
@@ -8,7 +8,14 @@ using System.Web.UI.WebControls;
 namespace Quiz {
     public partial class Quiz20210511_1 : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            string tb_ID = "";
+            string tb_Name = "";
+            if(Session["ID"] != null || Session["name"] != null)
+            {
+                tb_ID = (string)Session["ID"];
+                tb_Name = (string)Session["name"];
+                Response.Write(tb_ID +"<br />"+tb_Name);
+            }
         }
     }
 }
